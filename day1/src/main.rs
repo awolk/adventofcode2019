16e75a2677d84179df6ee189189143b864f06acd
Aidan Wolk <aidanwolk@comcast.net>
2019-12-02 01:30:11 -0800

    [day 1] include input and cleanup

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,45 +1,40 @@
-use std::io::{self, prelude::*};
-
-fn fuel_required(mass: i32) -> i32 {
+fn fuel_for_mass(mass: i32) -> i32 {
     (mass / 3) - 2
 }
 
-#[allow(dead_code)]
-fn part1() {
+fn part1(input: &str) {
     let fuel_required_sum: i32 =
-        io::stdin()
-            .lock()
+        input
             .lines()
-            .filter_map(|line| line.ok())
             .map(|line| line.parse().expect("expected integer"))
-            .map(fuel_required)
+            .map(fuel_for_mass)
             .sum();
 
     println!("Fuel required: {}", fuel_required_sum);
 }
 
-fn total_fuel_required(mass: i32) -> i32 {
-    let fuel_for_mass = fuel_required(mass);
+fn total_fuel_for_mass(mass: i32) -> i32 {
+    let fuel_for_mass = fuel_for_mass(mass);
     if fuel_for_mass < 0 {
         0
     } else {
-        fuel_for_mass + total_fuel_required(fuel_for_mass)
+        fuel_for_mass + total_fuel_for_mass(fuel_for_mass)
     }
 }
 
-fn part2() {
+fn part2(input: &str) {
     let fuel_required_sum: i32 =
-        io::stdin()
-            .lock()
+        input
             .lines()
-            .filter_map(|line| line.ok())
             .map(|line| line.parse().expect("expected integer"))
-            .map(total_fuel_required)
+            .map(total_fuel_for_mass)
             .sum();
 
     println!("Total fuel required: {}", fuel_required_sum);
 }
 
 fn main() {
-    part2()
+    let input = include_str!("input.txt");
+    part1(input);
+    part2(input)
 }
