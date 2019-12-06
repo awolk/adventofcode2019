16161f45a65a0b9f8bb5a8016aec180ee600a879
Aidan Wolk <aidanwolk@comcast.net>
2019-12-06 12:42:49 -0800

    [day 3] implement part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,6 @@
-use std::collections::HashSet;
-use std::convert::{TryFrom};
+use std::cmp::min;
+use std::collections::{HashMap, HashSet};
+use std::convert::TryFrom;
 
 enum Direction {
     Right,
@@ -53,15 +54,14 @@ fn parse_movements(line: &str) -> Result<Vec<Movement>, &'static str> {
     Ok(res)
 }
 
-fn manhattan_dist((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> i32 {
-    (x1 - x2).abs() + (y1 - y2).abs()
-}
-
-fn find_closest_intersection(input: &str) -> Result<(i32, i32), &'static str> {
+fn parse_wires(input: &str) -> Result<(Vec<Movement>, Vec<Movement>), &'static str> {
     let mut lines = input.lines();
     let wire1 = parse_movements(lines.next().ok_or("failed to get first line")?)?;
     let wire2 = parse_movements(lines.next().ok_or("failed to get second line")?)?;
+    Ok((wire1, wire2))
+}
 
+fn find_closest_intersection_dist(wire1: Vec<Movement>, wire2: Vec<Movement>) -> Option<i32> {
     let mut visited_positions = HashSet::new();
     visited_positions.insert((0, 0));
 
@@ -76,7 +76,6 @@ fn find_closest_intersection(input: &str) -> Result<(i32, i32), &'static str> {
         }
     }
 
-    let mut min_intersection = None;
     let mut min_intersection_distance = None;
 
     x = 0;
@@ -87,34 +86,75 @@ fn find_closest_intersection(input: &str) -> Result<(i32, i32), &'static str> {
             x += dx;
             y += dy;
             if visited_positions.contains(&(x, y)) {
-                let new_dist = manhattan_dist((x, y), (0, 0));
-                let is_new_min_intersection = match min_intersection_distance {
-                    None => true,
-                    Some(old_dist) => new_dist < old_dist
-                };
-
-                if is_new_min_intersection {
-                    min_intersection = Some((x, y));
-                    min_intersection_distance = Some(new_dist);
-                }
+                let new_dist = x.abs() + y.abs(); // manhattan distance from (0, 0)
+                min_intersection_distance = Some(match min_intersection_distance {
+                    None => new_dist,
+                    Some(old_dist) => min(old_dist, new_dist),
+                });
             }
         }
     }
 
-    match min_intersection {
-        None => Err("failed to find intersection"),
-        Some(pos) => Ok(pos)
-    }
+    min_intersection_distance
 }
 
 fn part1(input: &str) {
-    let pos = find_closest_intersection(input)
-        .expect("failed to find closest intersection");
-    let dist = manhattan_dist(pos, (0, 0));
-    println!("Part 1 result: {}", dist);
+    let (wire1, wire2) = parse_wires(input).expect("failed to parse wires");
+    let dist = find_closest_intersection_dist(wire1, wire2).expect("failed to find intersection");
+    println!("Part 1 distance: {}", dist);
+}
+
+fn find_min_signal_dist_on_intersection(wire1: Vec<Movement>, wire2: Vec<Movement>) -> Option<i32> {
+    // map of position to signal distance
+    let mut visited_positions = HashMap::new();
+    visited_positions.insert((0, 0), 0);
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut signal_dist = 0;
+    for movement in wire1 {
+        let (dx, dy) = movement.dir.to_delta();
+        for _ in 0..movement.dist {
+            x += dx;
+            y += dy;
+            signal_dist += 1;
+            visited_positions.entry((x, y)).or_insert(signal_dist);
+        }
+    }
+
+    let mut min_intersection_signal_dist = None;
+
+    x = 0;
+    y = 0;
+    signal_dist = 0;
+    for movement in wire2 {
+        let (dx, dy) = movement.dir.to_delta();
+        for _ in 0..movement.dist {
+            x += dx;
+            y += dy;
+            signal_dist += 1;
+            if let Some(dist_1) = visited_positions.get(&(x, y)) {
+                let new_dist = dist_1 + signal_dist;
+                min_intersection_signal_dist = Some(match min_intersection_signal_dist {
+                    None => new_dist,
+                    Some(old_dist) => min(old_dist, new_dist),
+                });
+            }
+        }
+    }
+
+    min_intersection_signal_dist
+}
+
+fn part2(input: &str) {
+    let (wire1, wire2) = parse_wires(input).expect("failed to parse wires");
+    let dist =
+        find_min_signal_dist_on_intersection(wire1, wire2).expect("failed to find intersection");
+    println!("Part 2 distance: {}", dist);
 }
 
 fn main() {
     let input = include_str!("input.txt");
     part1(input);
+    part2(input);
 }
