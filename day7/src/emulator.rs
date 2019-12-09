e9eca8c7e579862f48f88568b655338158637962
Aidan Wolk <aidanwolk@comcast.net>
2019-12-08 21:32:24 -0800

    [day 7] implement part 1

---
+++ day7/src/emulator.rs
@@ -0,0 +1,199 @@
+use std::sync::mpsc::{Receiver, SyncSender};
+
+enum ParameterMode {
+    Position,
+    Immediate,
+}
+
+enum Opcode {
+    Add,
+    Multiply,
+    Input,
+    Output,
+    Halt,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
+}
+
+struct Instruction {
+    opcode: Opcode,
+    p1_mode: ParameterMode,
+    p2_mode: ParameterMode,
+    p3_mode: ParameterMode,
+}
+
+impl Instruction {
+    fn parse_parameter_mode(mode: i32) -> Result<ParameterMode, &'static str> {
+        match mode {
+            0 => Ok(ParameterMode::Position),
+            1 => Ok(ParameterMode::Immediate),
+            _ => Err("invalid parameter mode"),
+        }
+    }
+
+    fn parse_opcode(opcode: i32) -> Result<Opcode, &'static str> {
+        Ok(match opcode {
+            1 => Opcode::Add,
+            2 => Opcode::Multiply,
+            3 => Opcode::Input,
+            4 => Opcode::Output,
+            5 => Opcode::JumpIfTrue,
+            6 => Opcode::JumpIfFalse,
+            7 => Opcode::LessThan,
+            8 => Opcode::Equals,
+            99 => Opcode::Halt,
+            _ => return Err("invalid opcode"),
+        })
+    }
+
+    fn parse(instr: i32) -> Result<Instruction, &'static str> {
+        let p3_mode = Instruction::parse_parameter_mode((instr / 10000) % 10)?;
+        let p2_mode = Instruction::parse_parameter_mode((instr / 1000) % 10)?;
+        let p1_mode = Instruction::parse_parameter_mode((instr / 100) % 10)?;
+        let opcode = Instruction::parse_opcode(instr % 100)?;
+
+        Ok(Instruction {
+            opcode,
+            p1_mode,
+            p2_mode,
+            p3_mode,
+        })
+    }
+}
+
+pub struct Emulator {
+    memory: Vec<i32>,
+    ip: i32,
+    halted: bool,
+
+    input: Receiver<i32>,
+    output: SyncSender<i32>,
+}
+
+impl Emulator {
+    pub fn new(
+        code: &str,
+        input: Receiver<i32>,
+        output: SyncSender<i32>,
+    ) -> Result<Emulator, &'static str> {
+        let memory = code
+            .split(',')
+            .map(|item| item.parse().map_err(|_| "failed to parse integer"))
+            .collect::<Result<Vec<i32>, &'static str>>()?;
+
+        Ok(Emulator {
+            memory,
+            ip: 0,
+            halted: false,
+            input,
+            output,
+        })
+    }
+
+    pub fn dup_memory(&self, input: Receiver<i32>, output: SyncSender<i32>) -> Emulator {
+        let memory = self.memory.clone();
+        Emulator {
+            memory,
+            ip: 0,
+            halted: false,
+            input,
+            output,
+        }
+    }
+
+    fn get(&self, address: i32) -> i32 {
+        self.memory[address as usize]
+    }
+
+    fn store(&mut self, address: i32, value: i32) {
+        self.memory[address as usize] = value;
+    }
+
+    fn get_arg_val(&self, n: i32, mode: ParameterMode) -> i32 {
+        let arg = self.get(self.ip + n);
+        match mode {
+            ParameterMode::Immediate => arg,
+            ParameterMode::Position => self.get(arg),
+        }
+    }
+
+    fn step(&mut self) -> Result<(), &'static str> {
+        let instr_code = self.get(self.ip);
+        let instr = Instruction::parse(instr_code)?;
+
+        match instr.opcode {
+            Opcode::Add => {
+                let arg1 = self.get_arg_val(1, instr.p1_mode);
+                let arg2 = self.get_arg_val(2, instr.p2_mode);
+                let res_addr = self.get(self.ip + 3);
+                self.store(res_addr, arg1 + arg2);
+                self.ip += 4;
+            }
+            Opcode::Multiply => {
+                let arg1 = self.get_arg_val(1, instr.p1_mode);
+                let arg2 = self.get_arg_val(2, instr.p2_mode);
+                let res_addr = self.get(self.ip + 3);
+                self.store(res_addr, arg1 * arg2);
+                self.ip += 4;
+            }
+            Opcode::Input => {
+                let input = self.input.recv().map_err(|_| "input failed")?;
+                let res_addr = self.get(self.ip + 1);
+                self.store(res_addr, input);
+                self.ip += 2;
+            }
+            Opcode::Output => {
+                let arg = self.get_arg_val(1, instr.p1_mode);
+                self.output.send(arg).map_err(|_| "output failed")?;
+                self.ip += 2;
+            }
+            Opcode::JumpIfTrue => {
+                let cond = self.get_arg_val(1, instr.p1_mode);
+                let dest = self.get_arg_val(2, instr.p2_mode);
+                if cond != 0 {
+                    self.ip = dest;
+                } else {
+                    self.ip += 3;
+                }
+            }
+            Opcode::JumpIfFalse => {
+                let cond = self.get_arg_val(1, instr.p1_mode);
+                let dest = self.get_arg_val(2, instr.p2_mode);
+                if cond == 0 {
+                    self.ip = dest;
+                } else {
+                    self.ip += 3;
+                }
+            }
+            Opcode::LessThan => {
+                let arg1 = self.get_arg_val(1, instr.p1_mode);
+                let arg2 = self.get_arg_val(2, instr.p2_mode);
+                let res_addr = self.get(self.ip + 3);
+                self.store(res_addr, (arg1 < arg2) as i32);
+                self.ip += 4;
+            }
+            Opcode::Equals => {
+                let arg1 = self.get_arg_val(1, instr.p1_mode);
+                let arg2 = self.get_arg_val(2, instr.p2_mode);
+                let res_addr = self.get(self.ip + 3);
+                self.store(res_addr, (arg1 == arg2) as i32);
+                self.ip += 4;
+            }
+            Opcode::Halt => {
+                self.halted = true;
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn run(&mut self) -> Result<(), &'static str> {
+        while !self.halted {
+            self.step()?
+        }
+
+        Ok(())
+    }
+}
---
+++ day7/src/main.rs
@@ -0,0 +1,101 @@
+use std::sync::mpsc::sync_channel;
+use std::thread::spawn;
+
+mod emulator;
+
+fn test_amps(amp_control_software: &str, phase_setting: [i32; 5]) -> Result<i32, &'static str> {
+    let (in1tx, in1rx) = sync_channel(0);
+    let (out1tx, out1rx) = sync_channel(0);
+    let mut em1 = emulator::Emulator::new(amp_control_software, in1rx, out1tx)?;
+
+    let (in2tx, in2rx) = sync_channel(0);
+    let (out2tx, out2rx) = sync_channel(0);
+    let mut em2 = em1.dup_memory(in2rx, out2tx);
+
+    let (in3tx, in3rx) = sync_channel(0);
+    let (out3tx, out3rx) = sync_channel(0);
+    let mut em3 = em1.dup_memory(in3rx, out3tx);
+
+    let (in4tx, in4rx) = sync_channel(0);
+    let (out4tx, out4rx) = sync_channel(0);
+    let mut em4 = em1.dup_memory(in4rx, out4tx);
+
+    let (in5tx, in5rx) = sync_channel(0);
+    let (out5tx, out5rx) = sync_channel(0);
+    let mut em5 = em1.dup_memory(in5rx, out5tx);
+
+    // start amplifiers
+    spawn(move || em1.run());
+    spawn(move || em2.run());
+    spawn(move || em3.run());
+    spawn(move || em4.run());
+    spawn(move || em5.run());
+
+    // send phase settings
+    in1tx.send(phase_setting[0]).map_err(|_| "send failed")?;
+    in2tx.send(phase_setting[1]).map_err(|_| "send failed")?;
+    in3tx.send(phase_setting[2]).map_err(|_| "send failed")?;
+    in4tx.send(phase_setting[3]).map_err(|_| "send failed")?;
+    in5tx.send(phase_setting[4]).map_err(|_| "send failed")?;
+
+    // connect pipeline
+    in1tx.send(0).map_err(|_| "send failed")?;
+    in2tx
+        .send(out1rx.recv().map_err(|_| "receive failed")?)
+        .map_err(|_| "send failed")?;
+    in3tx
+        .send(out2rx.recv().map_err(|_| "receive failed")?)
+        .map_err(|_| "send failed")?;
+    in4tx
+        .send(out3rx.recv().map_err(|_| "receive failed")?)
+        .map_err(|_| "send failed")?;
+    in5tx
+        .send(out4rx.recv().map_err(|_| "receive failed")?)
+        .map_err(|_| "send failed")?;
+
+    out5rx.recv().map_err(|_| "receive failed")
+}
+
+fn permutations() -> impl Iterator<Item = [i32; 5]> {
+    // uses Heap's algorithm, adapted from pseudocode on Wikipedia
+    let mut res: Vec<[i32; 5]> = Vec::with_capacity(120 /* 5! */);
+
+    let n = 5;
+    let mut a = [0, 1, 2, 3, 4];
+    let mut c = [0, 0, 0, 0, 0];
+
+    res.push(a);
+
+    let mut i = 0;
+    while i < n {
+        if c[i] < i {
+            if i % 2 == 0 {
+                a.swap(0, i);
+            } else {
+                a.swap(c[i], i);
+            }
+            res.push(a);
+            c[i] += 1;
+            i = 0;
+        } else {
+            c[i] = 0;
+            i += 1;
+        }
+    }
+
+    res.into_iter()
+}
+
+fn part1(amp_control_software: &str) {
+    let best_output = permutations()
+        .map(|phase_setting| test_amps(amp_control_software, phase_setting).expect("failed test"))
+        .max()
+        .expect("failed to find max");
+
+    println!("Part 1: highest thruster signal = {}", best_output);
+}
+
+fn main() {
+    let amp_control_software = include_str!("input.txt");
+    part1(amp_control_software);
+}
